8eb8645b96e2defdcb309e6c18aa4c3822ebb2a4
SebastianC <secasval96@gmail.com>
2025-02-12 14:44:14 -0500

    Added 06-classes A

---
+++ src/main/java/org/example/cleanCode/Classes06A.java
@@ -0,0 +1,74 @@
+package org.example.cleanCode;
+
+import java.util.Date;
+
+public class Classes06A {
+
+    public enum Gender {
+        M, F
+    }
+
+    public static class Person {
+        public String name;
+        public Gender gender;
+        public Date birthdate;
+
+        public Person(String name, Gender gender, Date birthdate) {
+            this.name = name;
+            this.gender = gender;
+            this.birthdate = birthdate;
+        }
+    }
+
+    public static class User extends Person {
+        public Date lastAccess;
+
+        public User(String name, Gender gender, Date birthdate) {
+            super(name, gender, birthdate);
+            this.lastAccess = new Date();
+        }
+
+        public boolean checkCredentials(){
+            return true;
+        }
+    }
+
+    public static class UserSettings extends User{
+        public String workingDirectory;
+        public String lastOpenFolder;
+
+        public UserSettings(String name,
+                            Gender gender,
+                            Date birthdate,
+                            String workingDirectory,
+                            String lastOpenFolder) {
+            super(name, gender, birthdate);
+            this.workingDirectory = workingDirectory;
+            this.lastOpenFolder = lastOpenFolder;
+        }
+
+        @Override
+        public String toString() {
+            return "UserSettings{" +
+                    "workingDirectory='" + workingDirectory + '\n' +
+                    ", lastOpenFolder='" + lastOpenFolder + '\n' +
+                    ", lastAccess=" + lastAccess +
+                    ", name='" + name + '\n' +
+                    ", gender=" + gender +
+                    ", birthdate=" + birthdate +
+                    '}';
+        }
+    }
+
+    public static void main(String[] args) {
+
+        UserSettings userSettings = new UserSettings("Jhoan",
+                Gender.M,
+                new Date(),
+                "usr/home/",
+                "/home"
+                );
+
+        System.out.println(userSettings);
+    }
+}
